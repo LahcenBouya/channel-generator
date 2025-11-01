72216b48a487949359bf7175a4170cd671aa2073
lahcen bouya <lahcenbouya2007@gmail.com>
2025-11-01 21:52:21 +0800

    Add files via upload

---
+++ channel name.py
@@ -0,0 +1,4 @@
+print("welcome to the chanel name generater")
+nikname=input("what is your nickname:\n")
+channel=input("what is your chaneel about:\n")
+print("you could name your channel ", "("+channel + " with " + nikname +")")
